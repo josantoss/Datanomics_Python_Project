fc0cc9371a071d6eb1f6b33184763fe092f2a2e4
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2025-11-23 00:00:11 +0300

    Vending Machine Programme

---
+++ Vending_Machine.py
@@ -0,0 +1,12 @@
+# vending Machine Program
+
+menu = {
+    "1": ("Chocolate Bar", 1.50),
+    "2": ("Chips", 1.20),
+    "3": ("Candy", 0.90),
+    "4": ("Soda", 2.00),
+    "5": ("Water", 1.00),
+    "6": ("Cookies", 2.50),
+    "7": ("Gum", 0.75),
+    "8": ("Popcorn", 1.80)
+}
